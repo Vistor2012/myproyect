6060066e933171da1aa8a628db1b015236b5c388
Vistor2012 <vhchoque43qgmail.com>
2018-07-11 11:57:54 -0400

    corrigiendoooo

--- app/src/main/java/com/example/victor/myproyect/DATA/DataApp.java
+++ app/src/main/java/com/example/victor/myproyect/DATA/DataApp.java
@@ -8,7 +8,7 @@ public class DataApp {
 
     static public ArrayList<ItemList>LISTINFO;
 
-    static public String HOST = "http://192.168.1.3:7777/api/v1.0/";
+    static public String HOST = "http://192.168.100.75:7777/api/v1.0/";
     static public String HOST_INMUEBLE = HOST+"inmuebles/";
     static public String HOST_ROOT = HOST+"agenteVentas/";
 
